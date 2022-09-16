2f6a199fe5f884e7eef34099c94aaa02e2950c4d
nghialv <nghialv2607@gmail.com>
2022-09-16 13:51:51 +0700

    Add api implementation

--- pkg/api/greeting/api.go
+++ pkg/api/greeting/api.go
@@ -2,20 +2,33 @@ package greeting
 
 import (
 	"context"
+	"fmt"
+	"sync"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	"github.com/nghialv/gateway/pkg/model"
 	service "github.com/nghialv/gateway/pkg/service/greeting/v2"
 )
 
 type GreetingAPI struct {
 	service.UnimplementedGreetingServiceServer
+
+	mu       sync.RWMutex
+	database map[string]model.Message
+	counter  int
 }
 
 func NewAPI() *GreetingAPI {
-	return &GreetingAPI{}
+	api := &GreetingAPI{
+		mu:       sync.RWMutex{},
+		database: make(map[string]model.Message, 0),
+		counter:  0,
+	}
+	return api
 }
 
 func (a *GreetingAPI) Register(server *grpc.Server) {
@@ -23,9 +36,33 @@ func (a *GreetingAPI) Register(server *grpc.Server) {
 }
 
 func (a *GreetingAPI) GetMessage(ctx context.Context, req *service.GetMessageRequest) (*service.GetMessageResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "")
+	a.mu.RLock()
+	defer a.mu.Unlock()
+
+	msg, ok := a.database[req.Id]
+	if !ok {
+		return nil, status.Error(codes.NotFound, "")
+	}
+
+	return &service.GetMessageResponse{
+		Message: &msg,
+	}, nil
 }
 
 func (a *GreetingAPI) SendMessage(ctx context.Context, req *service.SendMessageRequest) (*service.SendMessageResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "")
+	a.counter++
+	msg := model.Message{
+		Id:        fmt.Sprintf("%d", a.counter),
+		Content:   req.Content,
+		Sender:    "anonymous",
+		CreatedAt: uint64(time.Now().Unix()),
+	}
+
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	a.database[msg.Id] = msg
+
+	return &service.SendMessageResponse{
+		Message: &msg,
+	}, nil
 }
