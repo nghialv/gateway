5784e10308ef79668b0a200e44977dff61da66bc
nghialv <nghialv2607@gmail.com>
2022-09-16 12:50:48 +0700

    Add api

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -3,20 +3,24 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"log"
+	"net"
 	"net/http"
 
-	"github.com/golang/glog"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
-	svc "github.com/nghialv/gateway/pkg/service/greeting/v2"
+	greetingapi "github.com/nghialv/gateway/pkg/api/greeting"
+	greetingsvc "github.com/nghialv/gateway/pkg/service/greeting/v2"
 )
 
 var (
 	// command-line options:
 	// gRPC server endpoint
 	grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:9090", "gRPC server endpoint")
+	port               = flag.Int("port", 9090, "The server port")
 )
 
 func run() error {
@@ -24,12 +28,27 @@ func run() error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	server := grpc.NewServer()
+	api := greetingapi.NewAPI()
+	api.Register(server)
+
+	go func() {
+		log.Printf("server listening at %v", lis.Addr())
+		if err := server.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
 	// Register gRPC server endpoint
 	// Note: Make sure the gRPC server is running properly and accessible
 	mux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-	err := svc.RegisterGreetingServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
-	if err != nil {
+	if err := greetingsvc.RegisterGreetingServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts); err != nil {
 		return err
 	}
 
@@ -39,9 +58,8 @@ func run() error {
 
 func main() {
 	flag.Parse()
-	defer glog.Flush()
 
 	if err := run(); err != nil {
-		glog.Fatal(err)
+		log.Fatal(err)
 	}
 }
---
+++ pkg/api/greeting/api.go
@@ -0,0 +1,27 @@
+package greeting
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	service "github.com/nghialv/gateway/pkg/service/greeting/v2"
+)
+
+type GreetingAPI struct {
+	service.UnimplementedGreetingServiceServer
+}
+
+func NewAPI() *GreetingAPI {
+	return &GreetingAPI{}
+}
+
+func (a *GreetingAPI) Register(server *grpc.Server) {
+	service.RegisterGreetingServiceServer(server, a)
+}
+
+func (a *GreetingAPI) SayHello(ctx context.Context, req *service.SayHelloRequest) (*service.SayHelloResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
